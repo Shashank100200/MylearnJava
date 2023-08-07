1ef09935c5ff547a0248c832a25bc827a3d5e7d0
Shashank <shashankbn2023@gmail.com>
2023-08-07 22:40:35 +0530

    Initial commit

--- src/Mylearn.java
+++ src/Mylearn.java
@@ -1,5 +1,6 @@
 public class Mylearn {
     public static void main(String[] args){
-        System.out.print("Hello World");
+        System.out.print("Hello World\nMy name is Shashank\nI love Java");
     }
 }
+
---
+++ src/java.java
@@ -0,0 +1,134 @@
+import java.util.*;
+import java.util.Scanner;
+
+//public class java{
+//    public static void main(String[] args){
+//        int a = 10;
+//        int b = 13;
+//        double ans = (a*b)/(a+b);
+//        System.out.println(ans);
+//    }
+//}
+
+//public class java{
+//    public static void main(String[] args){
+//        Scanner x = new Scanner(System.in);
+////        String name = x.next(); // if there is only next it take only starting word
+//        String name = x.nextLine();
+//        System.out.print(name);
+//    }
+//}
+
+//public class java{
+//    public static void main(String[] args){
+//        Scanner x = new Scanner(System.in);
+//        System.out.println("Enter the radius :");
+//        float rad = x.nextFloat();
+//        System.out.println("The area of the circle is :" + 3.14 * rad * rad);
+//    }
+//}
+
+//public class java {
+//    public static void main(String[] args){
+//        Scanner x = new Scanner(System.in);
+//        System.out.print("Enter the age:");
+//        int age = x.nextInt();
+//        if (age>18){
+//            System.out.println("Yor are Adult");
+//        }
+//        else{
+//            System.out.println("You are not Adult");
+//        }
+//    }
+//}
+
+//public class java {
+//    public static void main(String[] args){
+//        Scanner x = new Scanner (System.in);
+//        System.out.println("Enter the number:");
+//        int num = x.nextInt();
+//        if(num%2 == 0){
+//            System.out.println("It is an odd number");
+//        }
+//        else{
+//            System.out.println("Its not an odd number");
+//        }
+//
+//    }
+//}
+
+
+//public class java{
+//    public static void main(String[] args){
+//        int a = 260;
+//        byte b = (byte)a;
+//        System.out.print(b);
+//    }
+//}
+
+//public class java{
+//    public static void main(String[] args){
+//        String x = "I Love Reva University";
+//        String y = "and Bangalore";
+//        System.out.println("Length of the string is:"+x.length());
+//        System.out.println("Capital"+x.toUpperCase());
+//        System.out.println("Capital"+x.toUpperCase());
+//        System.out.println("Concatinate :"+x +""+y);
+//        System.out.println(x.concat(y));
+//        System.out.println("Splitting :"+x.strip());
+//
+//
+//    }
+//}
+
+//public class java{
+//    public static void main(String[] args) {
+//        Scanner x = new Scanner(System.in);
+//        System.out.println("Enter the value of a:");
+//        float a = x.nextFloat();
+//        System.out.println("Enter the value of b:");
+//        float b = x.nextFloat();
+//
+//        if(a == b){
+//            System.out.println("Equal");
+//        }
+//        else if(a > b){
+//            System.out.println("a is greater");
+//        }
+//        else{
+//            System.out.println("a is lesser");
+//        }
+//    }
+//
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
